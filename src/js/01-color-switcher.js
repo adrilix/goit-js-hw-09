0f94dc2f7a20972084614dc80a4c0962bc862d7a
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2022-08-27 18:20:41 +0200

    1/3 HW9 JS done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const myBody = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+let timerId = null;
+
+startButton.addEventListener("click", changeBgc);
+
+stopButton.addEventListener("click", clearTimerId);
+
+function  changeBgc(event){
+    timerId = setInterval(()=>{
+        myBody.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+    startButton.disabled = true;
+    stopButton.disabled = false;
+}
+
+function clearTimerId(event){
+    clearInterval(timerId);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+} 
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
