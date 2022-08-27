25bf3c72ca1a082fe4d843d0896c025566f03e3a
adrilix <106884639+adrilix@users.noreply.github.com>
2022-08-27 16:21:14 +0000

    Deploying to gh-pages from @ adrilix/goit-js-hw-09@0f94dc2f7a20972084614dc80a4c0962bc862d7a 🚀

---
+++ 01-color-switcher.8e6dd50c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=null;e.addEventListener("click",(function(a){o=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(t){clearInterval(o),e.disabled=!1,n.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.8e6dd50c.js.map
