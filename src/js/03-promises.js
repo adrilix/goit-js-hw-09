d1acb119476fcebc1d904da5c50499a438f56508
Andrii Maliarenko <106884639+adrilix@users.noreply.github.com>
2022-08-28 21:30:59 +0200

    3/3 HW9 JS done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const refs = {
+  delay: document.querySelector('[name="delay"]'),
+  delayStep: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  btn: document.querySelector('[type="submit"]'),
+}
+
+refs.btn.addEventListener('click', onClickPromise);
+
+function onClickPromise(event) {
+  event.preventDefault();
+  const inputDelay = Number(refs.delay.value);
+  const inputDelayStep = Number(refs.delayStep.value);
+  const inputAmount = Number(refs.amount.value);
+
+  for (i = 1, delay = inputDelay; i <= inputAmount; i += 1, delay += inputDelayStep) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify
+          .success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify
+          .failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      })
   }
+};
+
+function createPromise(position, delay) {
+
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay)
+  })
 }
